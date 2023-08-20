3ab42b5774920884d25959b7f99da74e50524752
michaelwang <swangem@connect.ust.hk>
2023-08-20 21:11:44 +0800

    1d bottom up dp

--- 746.min-cost-climbing-stairs.cpp
+++ 746.min-cost-climbing-stairs.cpp
@@ -8,15 +8,18 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-         int n=cost.size();
-         vector<int>dp(n+1,0);
-         for(int i = 2;i <= n;i++){
-            dp[i]=min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
-         }
-        return dp[n];
+        int n = cost.size();
+        vector<int> costSum(n, 0);
+        costSum[0] = cost[0];
+        costSum[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            costSum[i] = cost[i] + min(costSum[i-1], costSum[i-2]);
+        }
+        return min(costSum[n-1], costSum[n-2]);
     }
 };
 // @lc code=end
 
 // walk: costSum + cost[i];
 // not walk: costSum[i-1];
+// mincost of lv i = cost[i] + min(costSum[i-1],costSum[i-2]);
